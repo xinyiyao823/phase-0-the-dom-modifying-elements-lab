25ff515d4e6204592c02f20bb9cc816b2ba1460e
Xinyi <xinyiyao84@yahoo.com>
2021-09-05 13:29:59 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector('main').remove();
-const newHeader = document.createElement('h1');
-newHeader.id= 'victory';
-newHeader.innerHTML = "YOUR-NAME is the champion"
+document.querySelector('main').remove(); // removed node "main" from document
+const newHeader = document.createElement('h1'); // creates new node h1
+newHeader.id= 'victory'; //sets id of h1 to victory
+newHeader.innerHTML = "YOUR-NAME is the champion" // writes to the page with new value inside h1
